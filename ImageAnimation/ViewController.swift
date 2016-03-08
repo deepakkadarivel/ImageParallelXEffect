02503bd206202bfb2b5593c1a3c1927634d61a91
Deepak <deepak.kadarivel@gmail.com>
2016-03-08 14:58:49 +0530

    animating image in background giving parallelx effect

--- ImageAnimation/ViewController.swift
+++ ImageAnimation/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +21,20 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewDidAppear(animated: Bool) {
+        animateClouds(imageView)
+    }
 
+    func animateClouds(image: UIImageView) {
+        let cloudMovingSpeed = 30.0 / view.frame.size.width
+        let duration = (view.frame.size.width - image.frame.origin.x) * cloudMovingSpeed
+        UIView.animateWithDuration(NSTimeInterval(duration), delay: 0.0, options: [.CurveLinear, .Repeat, .Autoreverse], animations: { () -> Void in
+            image.frame = CGRect(x: self.view.frame.origin.x, y: self.view.frame.origin.y, width: image.frame.width + 100, height: image.frame.height + 100)
+            }) { _ in
+                image.frame.origin.x = 0
+                self.animateClouds(image)
+        }
+    }
 
 }
-
